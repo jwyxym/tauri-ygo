9ff9698338cf67b0e11a32c38045f1f5fbef00b5
jwyxym <jwyxym@126.com>
2025-08-24 21:08:10 +0800

    add vue3-starry-sky

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+import Vue3StarrySky from 'vue3-starry-sky';
+import 'vue3-starry-sky/lib/style.css';
 const app = createApp(App)
 
 app.use(Toast, {})
+app.use(Vue3StarrySky);
 
 app.mount('#app')
