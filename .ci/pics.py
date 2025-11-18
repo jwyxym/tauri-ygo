4b2cb2ab337d3b0dce376aa03022bc145158565b
jwyxym <jwyxym@126.com>
2025-11-18 14:13:43 +0800

    fix

--- .ci/pics.py
+++ .ci/pics.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
-response = requests.request("GET", "https://ygom.top/")
-print(response.status_code)
+response = requests.request("GET", "https://api.jwyxym.top/web/ygom")
 if response.ok:
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -11,5 +10,4 @@ if response.ok:
     if elem:
         link = elem.get('href')
         if link:
-            print(link)
             os.system(f"wget -O YGOMobile.apk {link} ")
