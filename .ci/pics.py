3d14622f21d5e1fe1b6121a76b9f357528fc99d0
jwyxym <jwyxym@126.com>
2025-11-21 11:02:04 +0800

    Revert "fix"
    This reverts commit 4b2cb2ab337d3b0dce376aa03022bc145158565b.

--- .ci/pics.py
+++ .ci/pics.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
-response = requests.request("GET", "https://api.jwyxym.top/web/ygom")
+response = requests.request("GET", "https://ygom.top/")
+print(response.status_code)
 if response.ok:
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -10,4 +11,5 @@ if response.ok:
     if elem:
         link = elem.get('href')
         if link:
+            print(link)
             os.system(f"wget -O YGOMobile.apk {link} ")
