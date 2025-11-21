6d9a41a7012edd5eb36087bd23b56fa3c4e941e4
jwyxym <jwyxym@126.com>
2025-11-21 11:01:59 +0800

    Revert "Update pics.py"
    This reverts commit 72ef54c3cc140e8739154e2f7649311fae78b61e.

--- .ci/pics.py
+++ .ci/pics.py
@@ -10,4 +10,4 @@ if response.ok:
     if elem:
         link = elem.get('href')
         if link:
-            os.system(f'wget --header="Referer: https://ygom.top/" --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36" -O YGOMobile.apk {link}')
+            os.system(f"wget -O YGOMobile.apk {link} ")
