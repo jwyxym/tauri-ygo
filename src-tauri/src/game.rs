4da2ba978d500f8111cac30da7366a9f856ce0a1
jwyxym <jwyxym@126.com>
2026-02-14 14:39:26 +0800

    fix

--- src-tauri/src/game.rs
+++ src-tauri/src/game.rs
@@ -3,18 +3,20 @@ mod system;
 mod server;
 mod card;
 mod pic;
+mod zip;
 use strings::Strings;
 use system::System;
 use server::Server;
 use card::Card;
 use pic::Pic;
+use zip::Zip;
 
+use tauri_plugin_os::{OsType, type_};
 use urlencoding::encode;
 use std::fs::read_to_string;
 use serde::{Serialize, Deserialize};
-use tauri_plugin_os::{OsType, type_};
-
 use anyhow::{Result, Error};
+use walkdir::WalkDir;
 
 #[derive(Serialize, Clone, Debug)]
 pub struct Game {
@@ -37,10 +39,33 @@ impl Game  {
 	}
 
 	pub async fn init () -> Result<(), Error> {
-		let mut strings: Strings = Strings::new();
-		//这里得写一个绝对路径用于调试
-		let text: String = read_to_string("E://strings.conf")?;
-		strings.init(text);
+		let start = std::time::Instant::now();
+		let mut tasks = Vec::new();
+		for entry in WalkDir::new(r"E:\github\Rust\tauri-ygo\src-tauri\target\debug\expansions") {
+			if let Ok(e) = entry {
+				let path = e.path();
+				if path.is_file() {
+					if let Some(stem) = path.file_stem().and_then(|n| n.to_str()) {
+						if let Some(ext) = path.extension().and_then(|n| n.to_str()) {
+							if let Some(path) = path.as_os_str().to_str() {
+								println!("{} {} {}", ext.to_string(), stem.to_string(), path.to_string());
+								tasks.push(Zip::open(path.to_string(), None));
+							}
+						}
+					}
+				}
+			}
+		}
+		for task in tasks {
+			let zip = task.await??;
+			// println!("{:?}", zip);
+		}
+		let duration = start.elapsed();
+		println!("耗时: {:?}", duration);
+		// let mut strings: Strings = Strings::new();
+		// //这里得写一个绝对路径用于调试
+		// let text: String = read_to_string("E://strings.conf")?;
+		// strings.init(text);
 		// println!("{:?}", strings);
 		Ok(())
 	}
