fdd8dde8c355fdeae56dfad5360804c7ddadd3b0
jwyxym <jwyxym@126.com>
2026-09-10 19:39:46 +0800

    增加卡片列表搜索

--- src/pages/card/card_list.vue
+++ src/pages/card/card_list.vue
@@ -44,6 +44,10 @@
 				</transition>
 				<div class = 'button'>
 					<Button
+						:content = 'mainGame.get.text(I18N_KEYS.DECK_BTN_SEARCH)'
+						@click = 'search.show = true'
+					/>
+					<Button
 						:content = 'mainGame.get.text(I18N_KEYS.EXIT)'
 						:loading = 'input.loading'
 						@click = "emit('exit')"
@@ -52,19 +56,47 @@
 				</div>
 			</main>
 		</transition>
+		<transition name = 'opacity'>
+			<Searcher
+				v-if = 'search.show'
+				v-model:ot = 'search.info.ot'
+				v-model:type = 'search.info.type'
+				v-model:attribute = 'search.info.attribute'
+				v-model:race = 'search.info.race'
+				v-model:category = 'search.info.category'
+				v-model:link = 'search.info.link'
+				v-model:lflist = 'search.info.lflist'
+				v-model:forbidden = 'search.info.forbidden'
+				v-model:lv = 'search.info.lv'
+				v-model:atk = 'search.info.atk'
+				v-model:def = 'search.info.def'
+				v-model:scale = 'search.info.scale'
+				v-model:desc = 'input.value'
+				v-model:type-switch = 'search.switchs.type'
+				v-model:category-switch = 'search.switchs.category'
+				v-model:link-switch = 'search.switchs.link'
+				@search = 'input.confirm'
+				@clear = 'search.clear'
+				@close = 'search.show = false'
+			/>
+		</transition>
 	</div>
 </template>
 <script setup lang = 'ts'>
-	import { computed, reactive, onMounted } from 'vue';
+	import { computed, reactive, onMounted, nextTick } from 'vue';
 	import { RecycleScroller } from 'vue-virtual-scroller';
 
 	import Button from '@/ui/button.vue';
 	import Input from '@/ui/input.vue';
+	import Searcher from '@/pages/deck/searcher.vue';
 
 	import mainGame from '@/script/game';
 	import GLOBAL from '@/script/scale';
 	import { I18N_KEYS } from '@/script/language/i18n';
 	import Card from '@/script/card';
+	import { KEYS } from '@/script/constant';
+	import Search from '@/pages/deck/search';
+
 
 	import Card_Info from './card_info.vue';
 
@@ -85,32 +117,75 @@
 		}
 	});
 
+	const search = reactive({
+		show : false,
+		info : {
+			ot : [] as Array<number>,
+			type : [[], [], [], []] as [Array<number>, Array<number>, Array<number>, Array<number>],
+			attribute : [] as Array<number>,
+			race : [] as Array<number>,
+			category : [] as Array<number>,
+			link : [] as Array<number>,
+			lflist : mainGame.lflist.keys().next().value ?? KEYS.NA,
+			forbidden : '',
+			lv : '',
+			atk : '',
+			def : '',
+			scale : ''
+		},
+		switchs : { type : false, category : false, link : false },
+		clear : () : void => {
+			search.info.ot.length = 0;
+			search.info.type.forEach(i => i.length = 0);
+			search.info.attribute.length = 0;
+			search.info.race.length = 0;
+			search.info.category.length = 0;
+			search.info.link.length = 0;
+			search.info.lflist = '';
+			search.info.forbidden = '';
+			search.info.lv = '';
+			search.info.atk = '';
+			search.info.def = '';
+			search.info.scale = '';
+			input.clear();
+		}
+	});
+
 	const input = reactive({
 		loading : false,
 		value : '',
-		confirm : () : void => {
-			if (input.value) {
-				list.cards.length = 0;
-				const cards = mainGame.cards.values();
-				let c = cards.next();
-				while (!c.done) {
-					const card = c.value;
-					if (card.id.toString().includes(input.value) || card.name.includes(input.value))
-						list.cards.push(card);
-					c = cards.next();
-				}
-			} else
-				list.cards = Array.from(mainGame.cards.values());
-			return input.clear();
+		confirm : async () : Promise<void> => {
+			if (input.loading) return;
+			input.loading = true;
+			try {
+				const searcher = new Search()
+					.set.cards(Array.from(mainGame.cards.values()))
+					.set.ot(search.info.ot)
+					.set.type(search.info.type)
+					.set.race(search.info.race)
+					.set.attribute(search.info.attribute)
+					.set.category(search.info.category)
+					.set.link(search.info.link)
+					.set.lflist(search.info.lflist)
+					.set.forbidden(search.info.forbidden)
+					.set.lv(search.info.lv)
+					.set.scale(search.info.scale)
+					.set.atk(search.info.atk)
+					.set.def(search.info.def)
+					.set.desc(input.value)
+					.set.and_or(search.switchs);
+				list.cards = searcher.search();
+				await nextTick();
+			} finally {
+				input.loading = false;
+			}
 		},
 		clear : () : void => {
 			input.value = '';
 		}
 	});
 
-	onMounted(() => {
-		list.cards = Array.from(mainGame.cards.values());
-	});
+	onMounted(input.confirm);
 	const emit = defineEmits<{ exit : []; }>();
 
 </script>
@@ -118,7 +193,7 @@
 	.ygopro3__card {
 		height: 100%;
 		width: 100%;
-		[media = 'mobile'] & {
+		[media = 'mobile'] & > main {
 			:deep(.var-button) {
 				transform: scale(140%);
 				transform-origin: left top;
@@ -219,6 +294,12 @@
 				height: calc(100% - 50px);
 			}
 			.button {
+				display: flex;
+				flex-direction: column;
+				gap: 10px;
+				[media = 'mobile'] & {
+					gap: 50px;
+				}
 				position: absolute;
 				right: 0;
 				bottom: 0;
@@ -226,6 +307,10 @@
 					width: 90px;
 				}
 			}
+		}
+		> .ygopro3__deck__search {
+			position: absolute;
+			z-index: 1;
 		}
 	}
 	.opacity {
